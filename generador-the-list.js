b899390e19c7a2289e362752255a0112c8aa5181
FedericoA13 <56283793+FedericoA13@users.noreply.github.com>
2020-07-28 02:04:29 -0300

    Cambia el js
    No está bien hecho pero está cerca. Así como está, muestra en cada clic el headline 1, luego el 2, etc.

--- generador-the-list.js
+++ generador-the-list.js
@@ -7,20 +7,31 @@ const headlines = []
 const cantidad = document.querySelectorAll("p")
 console.log(cantidad.length);
 
+function numeroRandom(headlines){
+  headlines[Math.floor(Math.random() * headlines.length)]
+}
+
 const button = document.querySelector("#generador")
 
+
+for (let i = 0; i < cantidad.length; i++) {
+  headlines.push(cantidad[i].innerText);
+}
+
+console.log(headlines)
+
+
 button.onclick = function (){
-    for (let i = 0; i < cantidad.length; i++) {
-            headlines.push(cantidad[i].innerText);
-          }
-          console.log(headlines)
+    document.querySelector(".oculto").className = " ";
     const nodoPagina = document.querySelector('body');
     const espacioTexto = document.createElement('p');
-    const headline = document.createTextNode(headlines);
+    const headline = document.createTextNode(numeroRandom);
     espacioTexto.appendChild(headline);
-    nodoPagina.appendChild(espacioTexto) 
+    nodoPagina.appendChild(espacioTexto)
 
 }
+
+
 
 /*for (let i = 0; i < cantidad.length; i++) {
     console.log(cantidad[i].innerText);
