e680e0fb732ef17c272b3ae59f988ae405e1d354
FedericoA13 <56283793+FedericoA13@users.noreply.github.com>
2020-07-30 01:06:27 -0300

    Avanza el JS
    Saca un número random y lo asigna a los índices de la lista, solo me falta lograr que el html añada el texto.

--- generador-the-list.js
+++ generador-the-list.js
@@ -2,46 +2,64 @@
 
 console.log("Hola");
 
-const headlines = []
 
-const cantidad = document.querySelectorAll("p")
-console.log(cantidad.length);
+//console.log(headlines.length); //165 frases
+//const aleatorio = Number(Math.floor(Math.random()*(headlines.length)));
 
-function numeroRandom(headlines){
-  headlines[Math.floor(Math.random() * headlines.length)]
+/*for (let i = 0; i < headlines.length; i++) {
+  console.log(headlines[i].innerText);
 }
+*/
+
+
+/*function tituloRandom(headlines) {
+  for (let i = 0; i < headlines.length; i++) {
+    (headlines[i].innerText);
+  }
+  return Math.random() * (headlines);
+}
+*/
+
+// console.log(tituloRandom(headlines[Number]))
+
 
 const button = document.querySelector("#generador")
 
-
-for (let i = 0; i < cantidad.length; i++) {
-  headlines.push(cantidad[i].innerText);
-}
-
-console.log(headlines)
-
-
 button.onclick = function (){
-    document.querySelector(".oculto").className = " ";
-    const nodoPagina = document.querySelector('body');
-    const espacioTexto = document.createElement('p');
-    const headline = document.createTextNode(numeroRandom);
-    espacioTexto.appendChild(headline);
-    nodoPagina.appendChild(espacioTexto)
+  const headlines = document.querySelectorAll(".oculto")
 
+    let aleat = Math.random() * (headlines.length)
+    let aleatorios = Math.floor(aleat)
+    console.log(aleatorios)
+    let mostrar = console.log(headlines[aleatorios].innerText);
+    let mostrarNumero = headlines[aleatorios].innerText
+
+    while (Number (mostrarNumero) === Number(mostrar)){
+      document.querySelector(".oculto").className = " "; 
+      const nodoDiv = document.querySelector("#headline");
+      const texto = document.createTextNode(mostrar);
+      document.querySelector("#headline").appendChild(texto)
+    }
 }
 
+/*
+const nuevoParrafo = document.createElement('p');
+const textoTest = document.createTextNode("Armaniiii el taco no...")
+nuevoParrafo.appendChild(textoTest);
+document.querySelector("body").appendChild(nuevoParrafo)
 
+// Ahora coloco ese texto dentro del nuevo párrafo creado, y con appendChild los llevo a la página
+// Y con el .querySelector para el body, indico en qué parte del HTML va el nuevo párrafo. .appendChild es al final.
+*/
+
+
+
+// No me hace falta insertar texto. Como ya están ocultos en el html, con desocultar uno alcanza para que sea visible.
 
 /*for (let i = 0; i < cantidad.length; i++) {
     console.log(cantidad[i].innerText);
   }
 
-  const nodoPagina = document.querySelector('body');
-    const espacioTexto = document.createElement('p');
-    const headline = document.createTextNode(texto);
-    espacioTexto.appendChild(headline);
-    nodoPagina.appendChild(espacioTexto)
+  const 
 
     */
-   
